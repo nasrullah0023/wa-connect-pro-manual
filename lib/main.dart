02bb2d79cf1886373bf06b84047cc544bced66df
nasrullah0023 <muhnasrullah0023@gmail.com>
2025-05-06 20:05:02 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WA Connect Pro',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('WA Connect Pro Manual')),
+      body: const Center(child: Text('Halo, Anas! Aplikasi siap dikembangkan.')),
+    );
+  }
+}
